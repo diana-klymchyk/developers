af6598c4ab0b0591e8bc83f37c97a96cd1b20dd6
Diana Klymchyk <diana.klymchyk@gmail.com>
2025-02-28 01:16:25 +0200

    Implemented exchange rates API with caching & frontend table

--- fullstack/task/packages/server/src/app.module.ts
+++ fullstack/task/packages/server/src/app.module.ts
@@ -4,18 +4,26 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { graphqlConfig, typeormConfig } from './config';
 import { modules } from './entity-modules';
-import { ExchangeRateModule } from './services/exchange-rate/exchange-rate.module';
+// import { ExchangeRateModule } from './services/exchange-rate/exchange-rate.module';
+import { ExchangeRateModule } from './entity-modules/exchange-rate/exchange-rate.module';
+import { ExchangeRate } from './entity-modules/exchange-rate/exchange-rate.entity'; // ✅ Імпортуємо ентіті
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        TypeOrmModule.forRoot(typeormConfig),
+
+        TypeOrmModule.forRoot({
+            ...typeormConfig,
+            autoLoadEntities: true,
+        }),
         GraphQLModule.forRoot(graphqlConfig),
+
         ExchangeRateModule,
         ...modules,
     ],
+
     controllers: [],
 })
 export class AppModule {}
